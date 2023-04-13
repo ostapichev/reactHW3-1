24a437ff8fbb9b77e2bbffd83f1e37b9fdd7c391
Oleh <ytox@ukr.net>
2023-04-13 13:44:58 +0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import './App.css';
-import Simpsons from "./components/simpsons/Simpsons";
-import Rickandmarties from "./components/rickandmarties/Rickandmarties";
 
 function App() {
-  // 1. Описати всю сім'ю сімпсонів (5 персонажів)
-  // 2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
-  //https://rickandmortyapi.com/
-  //https://rickandmortyapi.com/api/character
-  //Створити 6 персонажів
-  let titleSimpsons = 'Simpsons';
-  let titleRickyMarties = 'Ricky and Marty';
+
   return (
       <div>
-        <h1>{titleSimpsons}</h1>
-        <Simpsons/>
-        <h1>{titleRickyMarties}</h1>
-        <Rickandmarties/>
       </div>
   );
 }
